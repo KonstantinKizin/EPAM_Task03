0eff9b43d4da4ca66c296a3e0cdf6b3e67d4d07d
OSmol <Olga_Smolyakova@outlook.com>
2017-11-23 12:58:28 +0300

    Update ControllerCommandService.java

--- src/main/java/service/ControllerCommandService.java
+++ src/main/java/service/ControllerCommandService.java
@@ -3,6 +3,8 @@ package service;
 import service.exception.ServiceException;
 import java.util.Map;
 
-public interface ControllerCommandService extends Service {
+public interface ControllerCommandService extends Service {// вырви глаз название класса
+    // да и что оно делает на уровне сервисов
+    // зачем сервисам знать команды контроллера
     Map<String , String> getCommandMap() throws ServiceException;;
 }
